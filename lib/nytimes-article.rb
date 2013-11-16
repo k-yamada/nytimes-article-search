d1047d822fe4b036dfbe6bde4af0c655a03efa8f
Paul Schreiber <paul.schreiber@avaaz.org>
2013-11-16 16:49:15 -0500

    use v2 of the article search api

--- lib/nytimes-article.rb
+++ lib/nytimes-article.rb
@@ -7,9 +7,9 @@ require "nytimes-article/version"
 module NytimesArticle
   class << self
     API_SERVER  = 'api.nytimes.com'
-    API_VERSION = 'v1'
+    API_VERSION = 'v2'
     API_NAME    = 'search'
-    API_BASE    = "/svc/#{API_NAME}/#{API_VERSION}/article"
+    API_BASE    = "/svc/#{API_NAME}/#{API_VERSION}/articlesearch.json"
 
     @@api_key          = nil
     @@debug            = false
@@ -49,7 +49,7 @@ module NytimesArticle
         if @@api_key.nil?
           raise "You must initialize the API key before you run any API queries"
         end
-        full_params  = params.merge 'api-key' => @@api_key, 'format' => 'json'
+        full_params  = params.merge 'api-key' => @@api_key
         uri          = build_request_url(full_params)
         reply        = uri.read
         parsed_reply = JSON.parse reply
